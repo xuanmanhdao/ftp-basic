212f1eb112d30be8259db66a04bf01dde6e2f4f1
xuanmanhdao <xuanmanhdao2001@gmail.com>
2023-09-26 14:16:00 +0700

    Create menu feature ver 2

--- src/vn/ftpbasic/Main.java
+++ src/vn/ftpbasic/Main.java
@@ -8,25 +8,23 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+    private static Scanner scanner;
+    private static FTPConnectionManager ftpConnectionManager;
+    private static FTPTransfer ftpTransfer;
+    private static boolean isConnected;
+    private static FTPClient ftpClient;
 
     public static void main(String[] args) throws IOException {
         System.out.println("FTP Application");
 
-        Scanner scanner = new Scanner(System.in);
-
-        FTPConnectionManager ftpConnectionManager = new FTPConnectionManager();
-        FTPTransfer ftpTransfer = new FTPTransfer();
-
-        boolean isConnected = false;
-        FTPClient ftpClient = null;
+        scanner = new Scanner(System.in);
+        ftpConnectionManager = new FTPConnectionManager();
+        ftpTransfer = new FTPTransfer();
+        isConnected = false;
+        ftpClient = null;
 
         while (true) {
-            System.out.println("========== Menu ==========");
-            System.out.println("1. Connect to FTP server");
-            System.out.println("2. Get list of files on FTP server");
-            System.out.println("3. Download files from FTP server");
-            System.out.println("4. Upload file to FTP server");
-            System.out.println("0. Exit");
+            displayMenu();
 
             System.out.print("Enter your choice: ");
             int choice = scanner.nextInt();
@@ -34,89 +32,113 @@ public class Main {
 
             switch (choice) {
                 case 1:
-                    // Connect to FTP server
-                    if (!isConnected) {
-                        System.out.print("Enter FTP server address: ");
-                        String serverAddress = scanner.nextLine();
-
-                        System.out.print("Enter FTP server port: ");
-                        int serverPort = scanner.nextInt();
-                        scanner.nextLine(); // Đọc dòng mới sau khi đọc số
-
-                        System.out.print("Enter FTP server username: ");
-                        String username = scanner.nextLine();
-
-                        System.out.print("Enter FTP server password: ");
-                        String password = scanner.nextLine();
-
-                        System.out.print("Enter connection timeout (in milliseconds): ");
-                        int timeout = scanner.nextInt();
-                        scanner.nextLine(); // Đọc dòng mới sau khi đọc số
-
-                        try {
-                            ftpConnectionManager.connectFTPServer(serverAddress, serverPort, username, password, timeout);
-                            ftpClient = ftpConnectionManager.getFtpClient();
-                            isConnected = true;
-                            System.out.println("Connected to FTP server successfully.");
-                        } catch (IOException e) {
-                            System.out.println("Failed to connect to FTP server: " + e.getMessage());
-                        }
-                    } else {
-                        System.out.println("Already connected to FTP server.");
-                    }
+                    connectToFTPServer();
                     break;
                 case 2:
-                    // Feature get list file FTP Server
-                    if (!isConnected) {
-                        System.out.println("Please connect to FTP server first.");
-                    } else {
-                        ftpTransfer.getListFile(ftpClient);
-                    }
+                    getListOfFiles();
                     break;
                 case 3:
-                    // Feature download files from FTP server
-                    if (!isConnected) {
-                        System.out.println("Please connect to FTP server first.");
-                    } else {
-                        System.out.print("Enter the number of files to download: ");
-                        int numFiles = scanner.nextInt();
-                        scanner.nextLine(); // Đọc dòng mới sau khi đọc số
-
-                        List<String> listFileNameNeedDownload = new ArrayList<>();
-                        for (int i = 0; i < numFiles; i++) {
-                            System.out.print("Enter the file name to download: ");
-                            String fileName = scanner.nextLine();
-                            listFileNameNeedDownload.add(fileName);
-                        }
-
-                        ftpTransfer.downloadFiles(ftpClient, listFileNameNeedDownload);
-                    }
+                    downloadFilesFromFTPServer();
                     break;
                 case 4:
-                    // Feature upload file to FTP server
-                    if (!isConnected) {
-                        System.out.println("Please connect to FTP server first.");
-                    } else {
-                        System.out.print("Enter the local file path to upload: ");
-                        String localFilePath = scanner.nextLine();
-                        ftpTransfer.uploadFile(ftpClient, localFilePath);
-                    }
+                    uploadFileToFTPServer();
                     break;
                 case 0:
-                    // Exit the program
-                    System.out.println("Exiting...");
-                    scanner.close();
-                    if (isConnected) {
-                        try {
-                            // Disconnect from FTP server
-                            ftpConnectionManager.disconnectFTPServer();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
+                    exitProgram();
                     return;
                 default:
                     System.out.println("Invalid choice. Please try again.");
+            }
+        }
+    }
+
+    private static void displayMenu() {
+        System.out.println("========== Menu ==========");
+        System.out.println("1. Connect to FTP server");
+        System.out.println("2. Get list of files on FTP server");
+        System.out.println("3. Download files from FTP server");
+        System.out.println("4. Upload file to FTP server");
+        System.out.println("0. Exit");
+    }
+
+    private static void connectToFTPServer() {
+        if (!isConnected) {
+            System.out.print("Enter FTP server address: ");
+            String serverAddress = scanner.nextLine();
+
+            System.out.print("Enter FTP server port: ");
+            int serverPort = scanner.nextInt();
+            scanner.nextLine(); // Đọc dòng mới sau khi đọc số
+
+            System.out.print("Enter FTP server username: ");
+            String username = scanner.nextLine();
+
+            System.out.print("Enter FTP server password: ");
+            String password = scanner.nextLine();
+
+            System.out.print("Enter connection timeout (in milliseconds): ");
+            int timeout = scanner.nextInt();
+            scanner.nextLine(); // Đọc dòng mới sau khi đọc số
+
+            try {
+                ftpConnectionManager.connectFTPServer(serverAddress, serverPort, username, password, timeout);
+                ftpClient = ftpConnectionManager.getFtpClient();
+                isConnected = true;
+                System.out.println("Connected to FTP server successfully.");
+            } catch (IOException e) {
+                System.out.println("Failed to connect to FTP server: " + e.getMessage());
+            }
+        } else {
+            System.out.println("Already connected to FTP server.");
+        }
+    }
+
+    private static void getListOfFiles() throws IOException {
+        if (!isConnected) {
+            System.out.println("Please connect to FTP server first.");
+        } else {
+            ftpTransfer.getListFile(ftpClient);
+        }
+    }
+
+    private static void downloadFilesFromFTPServer() {
+        if (!isConnected) {
+            System.out.println("Please connect to FTP server first.");
+        } else {
+            System.out.print("Enter the number of files to download: ");
+            int numFiles = scanner.nextInt();
+            scanner.nextLine(); // Đọc dòng mới sau khi đọc số
+
+            List<String> listFileNameNeedDownload = new ArrayList<>();
+            for (int i = 0; i < numFiles; i++) {
+                System.out.print("Enter the file name to download: ");
+                String fileName = scanner.nextLine();
+                listFileNameNeedDownload.add(fileName);
+            }
+
+            ftpTransfer.downloadFiles(ftpClient, listFileNameNeedDownload);
+        }
+    }
+
+    private static void uploadFileToFTPServer() throws IOException {
+        if (!isConnected) {
+            System.out.println("Please connect to FTP server first.");
+        } else {
+            System.out.print("Enter the local file path to upload: ");
+            String localFilePath = scanner.nextLine();
+            ftpTransfer.uploadFile(ftpClient, localFilePath);
+        }
+    }
+
+    private static void exitProgram() {
+        System.out.println("Exiting...");
+        scanner.close();
+        if (isConnected) {
+            try {
+                // Disconnect from FTP server
+                ftpConnectionManager.disconnectFTPServer();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
     }
